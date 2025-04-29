878a1acb9285e1af18f538fea59d5855e8f8754a
j3r0nimo <jerobalort@gmail.com>
2025-04-29 18:29:07 -0300

    29/4

--- ejercicio1.mongodb.js
+++ ejercicio1.mongodb.js
@@ -1,10 +1,15 @@
-db.empresa.insertMany([
-    {empId: 1, nombre: 'Clara', edad: 23, puesto: 'Ventas' },
-    {empId: 2, nombre: 'Ana', edad: 37, puesto: 'Seguridad' },
-    {empId: 3, nombre: 'Antonio', edad: 18, puesto: 'Pasante' }
-  ]);
-  
+use ('empresa')
+db.empleados.drop()
+db.empleados.insertMany([
+  {empId: 1, nombre: 'Clara', edad: 23, puesto: 'Ventas' },
+  {empId: 2, nombre: 'Ana', edad: 37, puesto: 'Seguridad' },
+  {empId: 3, nombre: 'Antonio', edad: 18, puesto: 'Pasante' }
+]);
 
-db.empresa.updateOne({nombre: 'Clara'}, {$set:{edad:26}})
-  
-db.empresa.deleteOne({puesto: 'Pasante'})
+
+db.empleados.updateOne({nombre: 'Clara'}, {$set:{edad:26}})
+
+db.empleados.deleteOne({puesto: 'Pasante'})
+
+db.empleados.find()
+
