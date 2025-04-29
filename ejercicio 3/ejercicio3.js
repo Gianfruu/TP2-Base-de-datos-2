1dbc42b06a5eebf852c5eea92f509f3c8f42cc52
Gianfranco <138410064+Gianfruu@users.noreply.github.com>
2025-04-29 19:46:28 -0300

    terminados

---
+++ ejercicio 3/ejercicio3.js
@@ -0,0 +1,7 @@
+db.empresa.insertMany([
+{_id: ObjectId('681041772c08aa1673d861e0'),  nombre: 'Jorge Olguin',edad: 31,  puesto: 'Encargado de Area'},
+{_id: ObjectId('681044cd2c08aa1673d861e1'),nombre: 'Ruben Pagnanini',edad: 27, puesto: 'Tecnico tornero'}
+
+]);
+
+db.empleados.find({}, { _id: 0, nombre: 1, puesto: 1 })
