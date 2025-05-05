8fdebd657f5a65fbd80832345651f711c5afa372
Gianfranco <138410064+Gianfruu@users.noreply.github.com>
2025-05-05 10:25:24 -0300

    Delete ejercicio 2 directory

--- ejercicio 2/ejercicio2.js
+++
@@ -1,8 +0,0 @@
-
-db.empresa.insertMany([
-{_id: ObjectId('681041772c08aa1673d861e0'),  nombre: 'Jorge Olguin',edad: 31,  puesto: 'Encargado de Area'},
-{_id: ObjectId('681044cd2c08aa1673d861e1'),nombre: 'Ruben Pagnanini',edad: 27, puesto: 'Tecnico tornero'}
-
-]);
-
-db.empleados.find({$and: [{edad: ($gte: 25)}, {edad: {$lte: 40}}]})
